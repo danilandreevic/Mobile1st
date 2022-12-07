8499b3955e71330edf23abe5a42ec8d7d9403f93
danilandreevic <danil125687@mail.ru>
2022-12-07 21:06:08 +0600

    2nd

--- First/App.js
+++ First/App.js
@@ -1,12 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, SafeAreaView, Button, Alert, Image } from 'react-native';
 
 export default function App() {
+  const handleTextPress = () => console.log('Ты нажал на кнопку')
+  const handleButtonPress = () => Alert.alert('Получишь результат','Основное сообщение',[
+    {text: "Да", onPress: () => alert('Ты нажал на кнопку да')},
+    {text:"Отмена", onPress: () => alert('Ты нажал на кнопку нет')}
+  ]);
+  const handleButtonPress2 = () => Alert.prompt('Молодец','Основное сообщение',text => alert(text))
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text numberOfLines={2} style={styles.text} onPress={(handleTextPress)}>Open up App.js to start {'\n'}working on your app!!!!!</Text>
+      <Button title={'Нажми на кнопку'} color='red' onPress={handleButtonPress}/>
+      <Button title={'Кнопка 2'} onPress={handleButtonPress2}/>
+
+      <Image source={require('./assets/adaptive-icon.png')}/>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
@@ -14,7 +25,9 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  text: {
+    color:'red',
+    margin: 100
   },
 });
